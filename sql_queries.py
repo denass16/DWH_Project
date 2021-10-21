3c35dd006d8767f3fc432619fed22d221afaa45c
denass16 <59085714+denass16@users.noreply.github.com>
2021-10-21 17:35:09 +0100

    Add files via upload

--- sql_queries.py
+++ sql_queries.py
@@ -123,28 +123,37 @@ staging_events_copy = ("""
 # FINAL TABLES
 
 songplay_table_insert = ("""
-        INSERT INTO songplay_table (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+        INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
         VALUES (%s, %s, %s, %s, %s,%s, %s, %s)
 """)
 
 user_table_insert = ("""
         INSERT INTO user_table
         VALUES (%s, %s, %s, %s, %s)
+        ON CONFLICT (user_id)
+        DO UPDATE
+        SET level = excluded.level
 """)
 
 song_table_insert = ("""
         INSERT INTO song_table
         VALUES (%s, %s, %s, %s, %s)
+        ON CONFLICT (song_id)
+        DO NOTHING
 """)
 
 artist_table_insert = ("""
         INSERT INTO artist_table
         VALUES (%s, %s, %s, %s, %s)
+        ON CONFLICT (artist_id)
+        DO NOTHING
 """)
 
 time_table_insert = ("""
         INSERT INTO time_table
         VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (start_time)
+        DO NOTHING
 """)
 
 song_select = ("SELECT song_id, song_table.artist_id FROM (song_table JOIN artist_table ON song_table.artist_id=artist_table.artist_id) \
@@ -155,4 +164,3 @@ song_select = ("SELECT song_id, song_table.artist_id FROM (song_table JOIN artis
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_songs_copy, staging_events_copy]
-#insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
